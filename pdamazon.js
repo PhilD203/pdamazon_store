02fad5cf17037354826b0c7b8edd09d2b1a56f6c
Phil Dugliss <phillipdugliss@gmail.com>
2019-05-24 20:46:11 -0500

    Basic Componets Complete
    added inquirer prompts.
    wrote database logic for selecting and updating products.

--- pdamazon.js
+++ pdamazon.js
@@ -13,5 +13,59 @@ let connection = mysql.createConnection({
   connection.connect(function(err){
       if (err) throw err;
       console.log("connected as id " + connection.threadId);
-    //call function here
+    prompt();
   });
+
+  function prompt(){
+    inquirer.prompt([
+        
+        {
+        message: "Would you like to buy something?",
+        name: "action",
+        type: "list",
+        choices: ["BUY ITEM"]
+         }
+    ])
+    .then(answers => {
+        console.log(answers.action);
+        if(answers.action == "BUY ITEM"){
+            buyItem();
+        }
+    })
+  }
+
+  function buyItem(){
+    connection.query("SELECT * FROM products ", (error,res) =>{
+        console.table(res);
+        inquirer.prompt([{
+            name: "action",
+            message: "What is the index of the item you would like?",
+            type:"number",
+            },
+        ])
+        .then(answers => {
+            inquirer.prompt([{
+                name: "quantity",
+                message: "How many would you like?",
+                type:"number",
+                },
+            ])
+                .then(result => {
+                select(res[answers.action] , result.quantity);
+            })
+        }
+    )}
+)};
+
+    function select(res , quantity){
+    if (res.stock_quantity < quantity){
+        console.log("We are all out. SORRY!")
+    }
+    else{
+        console.log("Your total is" , res.price * quantity , "dollars.")
+        let newQuantity = res.stock_quantity - quantity;
+        connection.query("UPDATE products SET `stock_quantity` = "+ newQuantity +"  WHERE `item_id` = "+ res.item_id , (error,resin) =>{
+            console.log("Thank You for Your Order!")
+        })
+    }
+}
--- pdamazonItemSeeds.sql
+++ pdamazonItemSeeds.sql
@@ -13,3 +13,18 @@ stock_quantity INTEGER NOT NULL,
 PRIMARY KEY (item_id)
 );
 
+INSERT INTO products
+    (product_name,department_name,price,stock_quantity)
+VALUES
+    ("Electric Goggles","Winter Sports",186,30),
+	("Soloman Huckknife Snowboard","Winter Sports",495,75),
+	("Capita Ultrafear","Winter Sports",395,75),
+	("Public Display Snowboard","Winter Sports",475,75),
+	("GNU Headspace Snowboard","Winter Sports",500,75),
+	("HOWL Gloves","Winter Sports",55,100),
+	("Volcom Jacket","Winter Sports",350,20),
+	("North Face Jacket","Winter Sports",450,20),
+	("Volcom Pants","Winter Sports",170,20),
+	("Thirty Two Boots","Winter Sports",189,20);
+
+
